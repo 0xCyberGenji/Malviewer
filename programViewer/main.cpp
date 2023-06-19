35dea8da09d647a3642531a098eaf9d28d8e4d32
Berkay Karakoç <devtborne@gmail.com>
2023-06-19 03:10:14 +0300

    Changed Actions in 95-114 Lines

--- programViewer/main.cpp
+++ programViewer/main.cpp
@@ -63,7 +63,7 @@ int main(int argc, char* argv[]) {
     DWORD filterFlags = FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_ATTRIBUTES |
         FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_CREATION | FILE_NOTIFY_CHANGE_SECURITY;
     FILE_NOTIFY_INFORMATION* notifyInfo;
-    HANDLE hMalwareDirectory = NULL;
+    HANDLE hMalwareDirectory, hProcess = NULL;
     char buf[1024];
 
     if (argc < 2) {
@@ -72,6 +72,7 @@ int main(int argc, char* argv[]) {
     }
     PID = atoi(argv[1]);
     printf("%s%s Target PID: %ld\n%s", sColor, s, PID, rColor);
+   
     wchar_t* fullPath = GetProcessFullPath(PID);
     if (fullPath == NULL) {
         printf("%s%s Failed to get process full path. Error Code: %lx%s\n", eColor, e, GetLastError(), rColor);
@@ -79,11 +80,11 @@ int main(int argc, char* argv[]) {
     }
     programPath = fullPath;
     RemoveFileNameFromPath(programPath);
-    printf("%s%s Program Path: %ls%s\n\n", iColor, i, programPath, rColor);
+    printf("%s%s Program Path: %ls%s\n", iColor, i, programPath, rColor);
 
     hMalwareDirectory = CreateFileW(programPath, FILE_LIST_DIRECTORY, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
     if (hMalwareDirectory == INVALID_HANDLE_VALUE) {
-        printf("Failed to open directory: %ls\n", programPath);
+        printf("Failed to open directory: %ls\n\n", programPath);
         free(fullPath);
         return EXIT_FAILURE;
     }
@@ -92,7 +93,10 @@ int main(int argc, char* argv[]) {
         notifyInfo = (FILE_NOTIFY_INFORMATION*)buf;
         while (notifyInfo) {
             if (notifyInfo->Action == FILE_ACTION_ADDED) {
-                printf("%s%s New File Added. File Name: %.*ls%s\n", iColor, i, notifyInfo->FileNameLength / sizeof(wchar_t), notifyInfo->FileName, iColor);
+                printf("%s%s | New File Added. File Name: %.*ls%s\n", iColor, i, notifyInfo->FileNameLength / sizeof(wchar_t), notifyInfo->FileName, iColor);
+                if (!(notifyInfo->Action == FILE_ACTION_MODIFIED)) {
+                    printf("%s%s |--> File Modified: %.*ls%s\n", iColor, i, notifyInfo->FileNameLength / 2, notifyInfo->FileName, rColor);
+                }
             }
             else if (notifyInfo->Action == FILE_ACTION_REMOVED) {
                 printf("%s%s File Removed:%.*ls%s\n", eColor, e, notifyInfo->FileNameLength / 2, notifyInfo->FileName, rColor);
@@ -104,13 +108,12 @@ int main(int argc, char* argv[]) {
                 printf("%s%s File Old Name: %.*ls%s\n", sColor, i, notifyInfo->FileNameLength / 2, notifyInfo->FileName, rColor);
             }
             else if (notifyInfo->Action == FILE_ACTION_MODIFIED) {
-                printf("%s%s File Modified:%.*ls%s\n", iColor, i, notifyInfo->FileNameLength / 2, notifyInfo->FileName, rColor);
+                printf("%s%s File Modified: %.*ls%s\n", eColor, i, notifyInfo->FileNameLength / 2, notifyInfo->FileName, rColor);
             }
             notifyInfo = notifyInfo->NextEntryOffset > 0 ? (FILE_NOTIFY_INFORMATION*)((LPBYTE)notifyInfo + notifyInfo->NextEntryOffset) : NULL;
         }
     }
     free(fullPath);
     CloseHandle(hMalwareDirectory);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
